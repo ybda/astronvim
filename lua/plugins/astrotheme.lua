818c4d87433f88ba415200eceb3abed2161dbdc2
Dmitriy B. <dbkkll@yandex.ru>
2024-04-26 15:44:39 +0500

    astrotheme.lua

---
+++ lua/plugins/astrotheme.lua
@@ -0,0 +1,17 @@
+---@type LazySpec
+return {
+    "AstroNvim/astrotheme",
+    opts = {
+        palettes = {
+            astrodark = { -- Extend or modify astrodarks palette colors
+                ui = {
+                    base = "#090909",
+                    inactive_base = "#0b0b0b",
+                    tool = "#0b0b0b",
+                    tabline = "#0b0b0b",
+                    current_line = "#291e27",
+                },
+            },
+        },
+    },
+}

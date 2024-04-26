b7da286da9afab77f3c2e8184d6f9008126abf08
Dmitriy B. <dbkkll@yandex.ru>
2024-04-26 16:50:41 +0500

    neo-tree: make dotfiles visible

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true, -- when true, they will just be displayed differently than normal items
+                hide_dotfiles = true,
+            },
+        },
+    },
+}

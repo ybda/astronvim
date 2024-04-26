2033b2dce248b5de3fbaae662cb0ee2b32dbff63
Dmitriy B. <dbkkll@yandex.ru>
2024-04-26 17:45:25 +0500

    Add autosave ignore: toml

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -25,6 +25,7 @@ return {
                 },
                 ignore_filetypes = { -- disable format on save for specified filetypes
                     -- "python",
+                    "toml",
                 },
             },
             disabled = { -- disable formatting capabilities for the listed language servers

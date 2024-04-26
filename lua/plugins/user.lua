0c6effd9b8944b9d69a31c9293e42cc64f86c75b
Dmitriy B. <dbkkll@yandex.ru>
2024-04-26 15:28:02 +0500

    user.lua basic config

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,19 +1,8 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- You can also add or configure plugins by creating files in this `plugins/` folder
 -- Here are some examples:
 
 ---@type LazySpec
 return {
-
-    -- == Examples of Adding Plugins ==
-
-    "andweeb/presence.nvim",
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "BufRead",
-        config = function() require("lsp_signature").setup() end,
-    },
 
     -- == Examples of Overriding Plugins ==
 
@@ -22,19 +11,7 @@ return {
         "goolord/alpha-nvim",
         opts = function(_, opts)
             -- customize the dashboard header
-            opts.section.header.val = {
-                " █████  ███████ ████████ ██████   ██████",
-                "██   ██ ██         ██    ██   ██ ██    ██",
-                "███████ ███████    ██    ██████  ██    ██",
-                "██   ██      ██    ██    ██   ██ ██    ██",
-                "██   ██ ███████    ██    ██   ██  ██████",
-                " ",
-                "    ███    ██ ██    ██ ██ ███    ███",
-                "    ████   ██ ██    ██ ██ ████  ████",
-                "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-                "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-                "    ██   ████   ████   ██ ██      ██",
-            }
+            opts.section.header.val = {}
             return opts
         end,
     },

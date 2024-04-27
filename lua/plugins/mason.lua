33476fed0ec9b2dbb8c0c3a0b763fd0fb1cd4b16
Dmitriy B. <dbkkll@yandex.ru>
2024-04-27 20:33:27 +0500

    Add shellcheck linter

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -21,6 +21,7 @@ return {
             opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
                 "prettier",
                 "stylua",
+                "shellcheck",
                 -- add more arguments for adding more null-ls sources
             })
         end,

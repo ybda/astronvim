ed215e2f30bbc034823417c1cb6a481802101bad
Dmitriy B. <dbkkll@yandex.ru>
2024-04-27 22:13:28 +0500

    Add mini-ai plugin

--- lua/community.lua
+++ lua/community.lua
@@ -22,4 +22,5 @@ return {
     { import = "astrocommunity.diagnostics.trouble-nvim" },
     { import = "astrocommunity.editing-support.undotree" },
     { import = "astrocommunity.motion.leap-nvim" },
+    { import = "astrocommunity.motion.mini-ai" },
 }
